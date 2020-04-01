5ced7fee4412bfdcb80c07b7856077753113ea72
mxu129 <60194598+mxu129@users.noreply.github.com>
2020-04-01 14:09:41 -0400

    j

--- class-examples/main.js
+++ class-examples/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
   console.log("we loaded"):
 
-  $("#title").css("color", #bada55);
+  $("button21").click(function() {
+
+  $("#title").click(function(){
+    $("#title").css("color", "#f9423a");
+    $("#title").css("font-size", "500px");
+  });
+
+  $("button2").click(function() {
+    $("title").removeClass("big-text");
+  });
+
+  $("p").mouseover(function() {
+    $("p").css("font-size", "21px");
+  });
+
+
 });
