b253407a5073e79711560624f0b5508936c38100
mxu129 <60194598+mxu129@users.noreply.github.com>
2020-04-08 12:52:21 -0400

    h

---
+++ hw/Jquery/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  console.log("we loaded");
+
+
+  $("#button1").click(function() {
+
+  $("#title").click(function(){
+    $("#title").css("color", "#f9423a");
+    $("#title").css("font-size", "500px");
+  });
+
+  $("button2").click(function() {
+    $("title").removeClass("big-text");
+  });
+
+  $("p").mouseover(function() {
+    $("p").css("font-size", "21px");
+  });
+
+$("p")
+  .mouseleave(function()) {
+
+    $("p")
+    .css("font-size", "16px");
+  }
+
+
+});
+
+$("#box").mousedown(function() {
+  $("#box").css(background-color", "green");
+
+});
+
+});
