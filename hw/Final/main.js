957f9f92a9987f2f0233a91dbbaf82ddb1b2c5bd
mxu129 <60194598+mxu129@users.noreply.github.com>
2020-05-06 18:38:33 -0400

    hh

---
+++ hw/Final/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  console.log("we loaded");
+
+  $(".grid1-text1").mouseover(function(){
+    $(".grid1-text1").css("font-size", "20px");
+  });
+
+
+
+    $(".grid2-photo").click(function(){
+      $(".pg").show(5000);
+    });
+
+
+$("body").css("background-color", "#fafad2");
+$("html").css("font-family", "Lacquer, cursive");
+
+});
